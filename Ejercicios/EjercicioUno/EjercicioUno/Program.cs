eed7dd18cf626a7e006feea7023205c5d8e6a408
enzo <enzo.ep@hotmail.com>
2023-08-28 22:32:03 -0300

    cambiasos

---
+++ Ejercicios/EjercicioUno/EjercicioUno/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EjercicioUno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculoUno();
+        }
+        static void calculoUno()
+        {
+            int cantidadNumeros = 5;
+            int[] numeros = new int[cantidadNumeros];
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                Console.Write($"Ingrese el número {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maximo = numeros[0];
+            int minimo = numeros[0];
+            int suma = numeros[0];
+
+            for (int i = 1; i < cantidadNumeros; i++)
+            {
+                maximo = Math.Max(maximo, numeros[i]);
+                minimo = Math.Min(minimo, numeros[i]);
+                suma += numeros[i];
+            }
+
+            double promedio = (double)suma / cantidadNumeros;
+
+            Console.WriteLine($"Valor máximo: {maximo}");
+            Console.WriteLine($"Valor mínimo: {minimo}");
+            Console.WriteLine($"Promedio: {promedio}");
+
+        }
+
+    }
+}
